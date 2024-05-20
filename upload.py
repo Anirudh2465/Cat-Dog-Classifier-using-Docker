ba3d7a3b7ab50ce44400152714b4b0cfff01e1f0
Anirudh2465 <110315983+Anirudh2465@users.noreply.github.com>
2024-05-20 21:28:56 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -3,7 +3,7 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 from keras.preprocessing.image import load_img
 from model import process_image, predict_class
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 photos = UploadSet('photos', IMAGES)
 
@@ -50,12 +50,12 @@ def upload():
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Prediction Result</title>
             </head>
-            <body style="font-family: Arial, sans-serif; background: linear-gradient(to bottom, #ffffff, #2a415a); display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
-                <div style="background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); width: 300px; text-align: center;">
+            <body style="font-family: Arial, sans-serif; background: linear-gradient(to bottom, #ffffff, #0056b3); display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+                <div style="background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 10 10 10px rgba(0, 0, 0, 0.1); width: 250px; text-align: center;">
                     <h1 style="font-size: 24px; margin-bottom: 20px;">Prediction Result</h1>
-                    <p>For <strong>{filename}</strong>:</p>
                     <p>The prediction is: <strong>{prediction}</strong></p>
                     <p>With probability: <strong>{percentage_float:.2%}</strong></p>
+                    <img scr="./static/img/{filename}">
                     <a href="/upload" style="background-color: #2a415a; color: #fff; padding: 10px 15px; text-decoration: none; border-radius: 5px;">Upload Another Image</a>
                 </div>
             </body>
@@ -68,5 +68,5 @@ def upload():
     # Web page to show before the POST request containing the image
     return render_template('upload.html')
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
